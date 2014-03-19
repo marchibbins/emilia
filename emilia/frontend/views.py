02340815bbcfd5c7803eb557ebd4be0a0e3b05de
Marc Hibbins <marchibbins@gmail.com>
2014-03-19 17:22:17 +0000

    List climbs in frontend index

--- emilia/frontend/views.py
+++ emilia/frontend/views.py
@@ -1,10 +1,13 @@
 from flask import Blueprint, render_template
 
+from emilia.climbs.models import Climb
+
 
 frontend = Blueprint('frontend', __name__)
 
 
 @frontend.route('/')
 def index():
-    """ Render a simple template. """
-    return render_template('frontend/index.html')
+    """ Render full list of Climbs. """
+    climbs = Climb.query.all()
+    return render_template('frontend/index.html', climbs=climbs)
