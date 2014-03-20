468bc811c47e95b00bb20e58b92082eca23c7985
Marc Hibbins <marchibbins@gmail.com>
2014-03-20 23:03:24 +0000

    Create model for Books with relationship to Climbs

--- emilia/climbs/fixtures.py
+++ emilia/climbs/fixtures.py
@@ -1,13 +1,22 @@
-from emilia.climbs.models import Climb
+from emilia.climbs.models import Book, Climb
 from emilia.extensions import db
 
 
 def install():
-    """ Adds Climb data to database. """
+    """ Adds Book and Climb data to database. """
+    books = [
+        ('100 Greatest Cycling Climbs',),
+        ('Another 100 Greatest Cycling Climbs',),
+    ]
+
+    for data in books:
+        book = Book(*data)
+        db.session.add(book)
+
     climbs = [
-        (1, 'Cheddar Gorge', 'Cheddar, Somerset'),
-        (2, 'Weston Hill', 'Weston, Bath'),
-        (3, 'Crowcombe Combe', 'Crowcombe, Somerset'),
+        (1, 'Cheddar Gorge', 'Cheddar, Somerset', 1),
+        (2, 'Weston Hill', 'Weston, Bath', 1),
+        (3, 'Crowcombe Combe', 'Crowcombe, Somerset', 1),
     ]
 
     for data in climbs:
--- emilia/climbs/models.py
+++ emilia/climbs/models.py
@@ -3,6 +3,35 @@ from sqlalchemy import Column
 from emilia.extensions import db
 
 
+class Book(db.Model):
+
+    """ Represents a single Book object. """
+
+    NAME_STR_MAX = 64
+
+    id = Column(db.Integer, primary_key=True)
+    name = Column(db.String(NAME_STR_MAX))
+
+    def serialize(self):
+        """ Returns the object as an easily serializeable object. """
+        return {
+            'id': self.id,
+            'name': self.name,
+        }
+
+    def __init__(self, name):
+        """ Populates model properties. """
+        self.name = name
+
+    def __repr__(self):
+        """ Returns the Book object representation. """
+        return '<Book %r>' % self.name
+
+    def __unicode__(self):
+        """ Returns a string representation of the Climb object. """
+        return '%s. %s' % (self.number, self.name)
+
+
 class Climb(db.Model):
 
     """ Represents a single Climb object. """
@@ -15,6 +44,9 @@ class Climb(db.Model):
     name = Column(db.String(NAME_STR_MAX))
     location = Column(db.String(LENGTH_STR_MAX))
 
+    book_id = db.Column(db.Integer, db.ForeignKey('book.id'))
+    book = db.relationship('Book', backref=db.backref('climbs', lazy='dynamic'))
+
     def serialize(self):
         """ Returns the object as an easily serializeable object. """
         return {
@@ -24,11 +56,12 @@ class Climb(db.Model):
             'location': self.location,
         }
 
-    def __init__(self, number, name, location):
+    def __init__(self, number, name, location, book_id):
         """ Populates model properties. """
         self.number = number
         self.name = name
         self.location = location
+        self.book_id = book_id
 
     def __repr__(self):
         """ Returns the Climb object representation. """
