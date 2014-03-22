5d3aafe30665a6e3e573888e05c5dfddb7014f02
Marc Hibbins <marchibbins@gmail.com>
2014-03-22 15:43:53 +0000

    Create Climb detail view

--- emilia/frontend/views.py
+++ emilia/frontend/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 
-from emilia.climbs.models import Book
+from emilia.climbs.models import Book, Climb
 
 
 frontend = Blueprint('frontend', __name__)
@@ -24,3 +24,10 @@ def book_detail(slug):
     """ Renders a detail view for the Book, matching slug. """
     book = Book.query.filter_by(slug=slug).first_or_404()
     return render_template('frontend/climbs/book_detail.html', book=book)
+
+
+@frontend.route('/books/<book_slug>/<climb_number>')
+def climb_detail(book_slug, climb_number):
+    """ Renders a detail view for the Climb, matching book slug and climb number. """
+    climb = Climb.query.join(Book).filter(Book.slug == book_slug, Climb.number == climb_number).first_or_404()
+    return render_template('frontend/climbs/climb_detail.html', climb=climb)
