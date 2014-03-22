911ffd166c1967c2d50c277d50a84ea4e7405e49
Marc Hibbins <marchibbins@gmail.com>
2014-03-22 14:30:33 +0000

    Create simple Book view and list Books on index

--- emilia/frontend/views.py
+++ emilia/frontend/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 
-from emilia.climbs.models import Climb
+from emilia.climbs.models import Book
 
 
 frontend = Blueprint('frontend', __name__)
@@ -8,6 +8,13 @@ frontend = Blueprint('frontend', __name__)
 
 @frontend.route('/')
 def index():
-    """ Render full list of Climbs. """
-    climbs = Climb.query.all()
-    return render_template('frontend/index.html', climbs=climbs)
+    """ Render full list of Books. """
+    books = Book.query.all()
+    return render_template('frontend/index.html', books=books)
+
+
+@frontend.route('/book/<slug>')
+def book_detail(slug):
+    """ Render detail view for a Book. """
+    book = Book.query.filter_by(slug=slug).first()
+    return book.long_name
