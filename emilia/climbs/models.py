c00b0223ae87d1479659191a96a24c81fe6c3a60
Marc Hibbins <marchibbins@gmail.com>
2014-03-22 16:09:58 +0000

    Add slug to Climb model

--- emilia/climbs/models.py
+++ emilia/climbs/models.py
@@ -42,10 +42,12 @@ class Climb(db.Model):
 
     """ Represents a single Climb object. """
 
+    SLUG_STR_MAX = 32
     NAME_STR_MAX = 64
     LENGTH_STR_MAX = 64
 
     id = Column(db.Integer, primary_key=True)
+    slug = Column(db.String(SLUG_STR_MAX), unique=True)
     number = Column(db.Integer)
     name = Column(db.String(NAME_STR_MAX))
     location = Column(db.String(LENGTH_STR_MAX))
@@ -62,8 +64,9 @@ class Climb(db.Model):
             'location': self.location,
         }
 
-    def __init__(self, number, name, location, book):
+    def __init__(self, number, slug, name, location, book):
         """ Populates model properties. """
+        self.slug = slug
         self.number = number
         self.name = name
         self.location = location
