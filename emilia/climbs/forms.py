e820d90f6a6a27a4585864ba9597cbf4d03b251b
Marc Hibbins <marchibbins@gmail.com>
2014-03-23 18:10:52 +0000

    Update Climb form validators

--- emilia/climbs/forms.py
+++ emilia/climbs/forms.py
@@ -10,9 +10,9 @@ class ClimbForm(Form):
     """ Primary Climb create and edit form. """
 
     slug = TextField(u'Slug', [DataRequired(), Length(max=Climb.SLUG_STR_MAX)], description=u'Url-safe identifier, for example: "zig-zag-hill"')
-    number = IntegerField(u'Climb number', [NumberRange(min=1)], description=u'For example: "101"')
+    number = IntegerField(u'Climb number', [DataRequired(), NumberRange(min=1)], description=u'For example: "101"')
     name = TextField(u'Climb name', [DataRequired(), Length(max=Climb.NAME_STR_MAX)], description=u'For example: "Leith Hill"')
     location = TextField(u'Full location', [DataRequired(), Length(max=Climb.LENGTH_STR_MAX)], description=u'For example: "Cheddar, Somerset"')
-    latitude = FloatField(u'latitude')
-    longitude = FloatField(u'latitude')
-    strava_id = IntegerField(u'Strava segment id', description='For example: "944629"')
+    latitude = FloatField(u'Latitude', [DataRequired()])
+    longitude = FloatField(u'Latitude', [DataRequired()])
+    strava_id = IntegerField(u'Strava segment id', [DataRequired()], description='For example: "944629"')
