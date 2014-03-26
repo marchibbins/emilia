7ec03ec0f7500a04a836381db9d6439240722c1c
Marc Hibbins <marchibbins@gmail.com>
2014-03-26 16:36:57 +0000

    Add unique validator to forms

--- emilia/climbs/forms.py
+++ emilia/climbs/forms.py
@@ -1,16 +1,32 @@
 from flask.ext.wtf import Form
 from wtforms import FloatField, IntegerField, TextField
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
-from wtforms.validators import DataRequired, Length, NumberRange
+from wtforms.validators import DataRequired, Length, NumberRange, ValidationError
 
 from emilia.climbs.models import Book, Climb, Region
+
+
+class UniqueValidator(object):
+
+    """ Validates uniqueness of field on model. """
+
+    def __init__(self, model, field, message=None):
+        self.model = model
+        self.field = field
+        if not message:
+            message = u'%s with this %s already exists.' % (model.__name__, field.name)
+        self.message = message
+
+    def __call__(self, form, field):
+        if self.model.query.filter(self.field == field.data).count() > 0:
+            raise ValidationError(self.message)
 
 
 class BookForm(Form):
 
     """ Primary Book create and edit form. """
 
-    slug = TextField(u'Slug', [DataRequired(), Length(max=Book.SLUG_STR_MAX)], description=u'Url-safe identifier, for example: "100-climbs"')
+    slug = TextField(u'Slug', [DataRequired(), UniqueValidator(Book, Book.slug), Length(max=Book.SLUG_STR_MAX)], description=u'Url-safe identifier, for example: "100-climbs"')
     short_name = TextField(u'Short name', [DataRequired(), Length(max=Book.NAME_STR_MAX)], description=u'For example: "100 Climbs"')
     long_name = TextField(u'Long name', [DataRequired(), Length(max=Book.NAME_STR_MAX)], description=u'For example: "100 Greatest Cycling Climbs: A Road Cyclist\'s Guide to Britain\'s Hills"')
 
@@ -19,7 +35,7 @@ class ClimbForm(Form):
 
     """ Primary Climb create and edit form. """
 
-    slug = TextField(u'Slug', [DataRequired(), Length(max=Climb.SLUG_STR_MAX)], description=u'Url-safe identifier, for example: "zig-zag-hill"')
+    slug = TextField(u'Slug', [DataRequired(), UniqueValidator(Climb, Climb.slug), Length(max=Climb.SLUG_STR_MAX)], description=u'Url-safe identifier, for example: "zig-zag-hill"')
     number = IntegerField(u'Climb number', [DataRequired(), NumberRange(min=1)], description=u'For example: "101"')
     name = TextField(u'Climb name', [DataRequired(), Length(max=Climb.NAME_STR_MAX)], description=u'For example: "Leith Hill"')
     location = TextField(u'Full location', [DataRequired(), Length(max=Climb.LENGTH_STR_MAX)], description=u'For example: "Cheddar, Somerset"')
@@ -35,5 +51,5 @@ class RegionForm(Form):
 
     """ Primary Region create and edit form. """
 
-    slug = TextField(u'Slug', [DataRequired(), Length(max=Region.SLUG_STR_MAX)], description=u'Url-safe identifier, for example: "south-west"')
+    slug = TextField(u'Slug', [DataRequired(), UniqueValidator(Region, Region.slug), Length(max=Region.SLUG_STR_MAX)], description=u'Url-safe identifier, for example: "south-west"')
     name = TextField(u'Name', [DataRequired(), Length(max=Region.NAME_STR_MAX)], description=u'For example: "South-west"')
