1e52139da18e29bc9df576d0c65d0dc9822938e9
Marc Hibbins <marchibbins@gmail.com>
2014-03-29 10:58:49 +0000

    Add Coverage to tests

--- test.py
+++ test.py
@@ -1,10 +1,27 @@
+from coverage import coverage
 import unittest
 
 
+def start_coverage():
+    """ Starts measuring code coverage. """
+    cov = coverage(branch=True, omit=['*/site-packages/*', 'test.py'])
+    cov.start()
+    return cov
+
+
+def stop_coverage(cov):
+    """ Stops measuring code coverage. """
+    # cov.report()
+    cov.html_report(directory='coverage')
+    cov.erase()
+
+
 def run():
-    """ """
+    """ Runs unittest runner and coverage. """
+    coverage = start_coverage()
     suite = unittest.TestLoader().discover('tests')
     unittest.runner.TextTestRunner(verbosity=2).run(suite)
+    stop_coverage(coverage)
 
 
 if __name__ == '__main__':
