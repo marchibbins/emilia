e0b68036bec78de6ca16e3a3bfaf2d70287a638f
Marc Hibbins <marchibbins@gmail.com>
2014-04-02 11:31:04 +0100

    Cache frontend views with default timeout

--- emilia/frontend/views.py
+++ emilia/frontend/views.py
@@ -2,18 +2,21 @@ from flask import Blueprint, render_template
 
 from emilia.climbs.models import Book, Climb
 from emilia.climbs.strava import strava
+from emilia.extensions import cache
 
 
 frontend = Blueprint('frontend', __name__)
 
 
 @frontend.route('/')
+@cache.cached()
 def index():
     """ Renders the Home page. """
     return render_template('frontend/index.html')
 
 
 @frontend.route('/books')
+@cache.cached()
 def book_list():
     """ Renders a full list of Books. """
     books = Book.query.all()
@@ -21,6 +24,7 @@ def book_list():
 
 
 @frontend.route('/books/<slug>')
+@cache.cached()
 def book_detail(slug):
     """ Renders a detail view for the Book, matching slug. """
     book = Book.query.filter_by(slug=slug).first_or_404()
@@ -28,6 +32,7 @@ def book_detail(slug):
 
 
 @frontend.route('/climbs/<slug>')
+@cache.cached()
 def climb_detail(slug):
     """ Renders a detail view for the Climb, matching slug. """
     climb = Climb.query.filter_by(slug=slug).first_or_404()
