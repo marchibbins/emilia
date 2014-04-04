10013100ed88e01b7141cd5becf12574c31ced0f
Marc Hibbins <marchibbins@gmail.com>
2014-04-04 18:12:02 +0100

    Install domready AMD module

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,6 +6,7 @@ module.exports = function(grunt) {
                 options: {
                     paths: {
                         'bonzo': 'lib/bonzo',
+                        'domready': 'lib/ready',
                         'qwery': 'lib/qwery',
                         '$': 'amd/$'
                     },
--- emilia/static/js/amd/app.js
+++ emilia/static/js/amd/app.js
@@ -1,4 +1,8 @@
-require(['$'], function($) {
+require(['$', 'domready'], function($, domready) {
     // Remove noscript class
     $('html').removeClass('js-off');
+
+    domready(function () {
+        console.log('domready');
+    });
 });
