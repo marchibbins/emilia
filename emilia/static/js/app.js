9fbcc17eeb5905546ba5edd04f201fa2befd4e4a
Marc Hibbins <marchibbins@gmail.com>
2014-05-10 08:02:49 +0100

    Init Angular Google Map on template

--- emilia/static/js/app.js
+++ emilia/static/js/app.js
@@ -29299,5 +29299,19 @@ MarkerWithLabel.prototype.setMap = function (theMap) {
   this.label.setMap(theMap);
 };;angular.module('emilia', ['google-maps'])
     .controller('emilia-map', function ($scope) {
-        console.log('Ready');
+
+        var DEFAULT_CENTER = {
+                latitude: 54.5,
+                longitude: -4
+            },
+
+            DEFAULT_DRAGGABLE = true;
+            DEFAULT_ZOOM = 6;
+
+        // Map config
+        $scope.map = {
+            center: DEFAULT_CENTER,
+            draggable: DEFAULT_DRAGGABLE,
+            zoom: DEFAULT_ZOOM
+        };
     });
--- emilia/static/js/modules/emilia.js
+++ emilia/static/js/modules/emilia.js
@@ -1,4 +1,18 @@
 angular.module('emilia', ['google-maps'])
     .controller('emilia-map', function ($scope) {
-        console.log('Ready');
+
+        var DEFAULT_CENTER = {
+                latitude: 54.5,
+                longitude: -4
+            },
+
+            DEFAULT_DRAGGABLE = true;
+            DEFAULT_ZOOM = 6;
+
+        // Map config
+        $scope.map = {
+            center: DEFAULT_CENTER,
+            draggable: DEFAULT_DRAGGABLE,
+            zoom: DEFAULT_ZOOM
+        };
     });
